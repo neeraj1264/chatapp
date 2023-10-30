b6cc8f69925f71a959cf8fa39ddc5e8ccc9391e9
Neeraj Manchanda <manchandaneeraj396@gmail.com>
2023-10-30 18:08:05 +0530

    s

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const path = require("path");
 
-const port = 3000;
+const port = 3001;
 const app = express();
 
 app.use(cors());
